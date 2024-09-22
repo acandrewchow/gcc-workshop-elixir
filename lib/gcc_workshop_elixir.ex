a6b7a5e13cd617ff02dbd639c0172f1d4cc1f9df
Andrew Chow <ac.andrewchow@gmail.com>
2024-09-22 00:59:24 -0400

    first commit

---
+++ lib/gcc_workshop_elixir.ex
@@ -0,0 +1,18 @@
+defmodule GccWorkshopElixir do
+  @moduledoc """
+  Documentation for `GccWorkshopElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GccWorkshopElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gcc_workshop_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule GccWorkshopElixirTest do
+  use ExUnit.Case
+  doctest GccWorkshopElixir
+
+  test "greets the world" do
+    assert GccWorkshopElixir.hello() == :world
+  end
+end
